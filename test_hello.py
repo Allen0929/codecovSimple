8ebae3279660e03b9e26c706270bdcb5a5078b43
imagayfish <79428374+imagayfish@users.noreply.github.com>
2021-04-11 20:33:07 -0400

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,11 @@
 import hello
 
 
-def test_says_world():
-    assert hello.say_what() == 'world'
+#def test_says_world():
+#assert hello.say_what() == 'world'
+
+def test_fibonacci_1(self):
+    self.assertEqual(fibonacci(1), 1)
+    
+def test_fibonacci_1(self):
+    self.assertEqual(fibonacci(10), 89)
