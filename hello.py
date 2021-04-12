369c178f74c16caf8496322e3147429e628fc79f
imagayfish <79428374+imagayfish@users.noreply.github.com>
2021-04-11 20:29:33 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,37 @@
 from __future__ import print_function
 import sys
 
+def fibonacci(n):
+    if n <= 1: 
+        return 1
+    else:
+        return fibonacci(n - 2) + fibonacci(n - 1)
+    
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+fibonacci(1)
+fibonacci(11)
 
-def hello(what):
-    print('Hello, {}!'.format(what))
+factorial(0)
+factorial(1)
+
+#def hello(what):
+#    print('Hello, {}!'.format(what))
 
 
-def say_what():
-    return 'world'
+#def say_what():
+#    return 'world'
 
 
-def main():
-    hello(say_what())
-    return 0
+#def main():
+#    hello(say_what())
+#   return 0
 
 
 #if __name__ == '__main__':
-#   sys.exit(main())
+#    sys.exit(main())
 
-say_what()
-hello(what)
-main()
